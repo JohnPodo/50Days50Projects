a377ded272ea11182a1d9cbaece5ff4d61eee314
Podojohn <johnpodogorianiotis@gmail.com>
2021-04-08 17:55:31 +0300

    Dad Jokes

---
+++ Number10/script.js
@@ -0,0 +1,19 @@
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  await fetch("https://icanhazdadjoke.com", config)
+    .then((res) => res.json())
+    .then((data) => {
+      jokeEl.innerHTML = data.joke;
+    });
+}
