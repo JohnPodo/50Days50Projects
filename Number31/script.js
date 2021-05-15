33d572af66074a64251faa4959853be60df8ecfa
podo-connect-line.gr1 <pod@connect-line.gr>
2021-05-15 20:06:42 +0300

    GoodCheapFast

---
+++ Number31/script.js
@@ -0,0 +1,23 @@
+const toggles = document.querySelectorAll(".toggle");
+const good = document.querySelector("#good");
+const fast = document.querySelector("#fast");
+const cheap = document.querySelector("#cheap");
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener("change", (e) => DoTheTrick(e.target));
+});
+
+function DoTheTrick(theClickedOne) {
+  if (good.checked && cheap.checked && fast.checked) {
+    if (good === theClickedOne) {
+      fast.checked = false;
+    }
+    if (cheap === theClickedOne) {
+      good.checked = false;
+    }
+
+    if (fast === theClickedOne) {
+      cheap.checked = false;
+    }
+  }
+}
